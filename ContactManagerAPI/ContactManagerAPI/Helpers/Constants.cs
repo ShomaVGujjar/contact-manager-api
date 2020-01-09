345e428201e070cd585a72de93f17441af818cf5
Will Velida <willvelida@hotmail.co.uk>
2020-01-09 06:08:55 +0000

    Adding DI and initial models

---
+++ ContactManagerAPI/ContactManagerAPI/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactManagerAPI.Helpers
+{
+    public class Settings
+    {
+        public const string COSMOS_DB_CONNECTION_STRING = "COSMOS_DB_CONNECTION_STRING";
+    }
+}
