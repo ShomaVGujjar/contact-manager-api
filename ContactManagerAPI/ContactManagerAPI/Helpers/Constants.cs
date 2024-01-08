432d7adf796d9b84802555441904fff245d6a62e
Shoma <shoma.gujjar@ixico.com>
2024-01-08 16:41:16 +0000

    #test

---
+++ ContactManagerAPI/ContactManagerAPI/Helpers/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactManagerAPI.Helpers
+{
+    public class Settings
+    {
+        public const string COSMOS_DB_CONNECTION_STRING = "CosmosDBConnection";
+        public const string DATABASE_NAME = "CosmosDBName";
+        public const string CONTAINER_NAME = "CosmosDBContainerName";
+    }
+}
---
+++ ContactManagerAPI/ContactManagerAPI/Helpers/Startup.cs
@@ -0,0 +1,32 @@
+﻿using ContactManagerAPI.Helpers;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace ContactManagerAPI.Helpers
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddFilter(level => true);
+            });
+
+            var config = builder.GetContext().Configuration;            
+
+            //var config = (IConfiguration)builder.Services.FirstOrDefault(d => d.ServiceType == typeof(IConfiguration))?.ImplementationInstance;
+
+            builder.Services.AddSingleton((s) => 
+            {
+                CosmosClientBuilder cosmosClientBuilder = new CosmosClientBuilder(config[Settings.COSMOS_DB_CONNECTION_STRING]);
+
+                return cosmosClientBuilder.WithConnectionModeDirect()
+                    .Build();
+            });
+        }
+    }
+}
